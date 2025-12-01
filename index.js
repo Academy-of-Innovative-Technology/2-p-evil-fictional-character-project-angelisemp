b2d2278a701960ea98f7c28b6245b8a2c2fb3408
Angelise Perez <angelisep3@nycstudents.net>
2025-12-01 12:40:17 -0500

    html&css

---
+++ index.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function(){
+});
+
+
+const pageOne = document.getElementById("pageOne");
+pageTwo.style.display = "none";
+pageOne.style.display = "block";
